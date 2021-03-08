5408035b1ff1be40f9a8c5bf02b61713c17bf960
mahmudunnabiRain <mahmudunnabirain@gmail.com>
2021-03-08 14:59:50 +0600

    Image cropper integrated

--- FitNass/wwwroot/js/site.js
+++ FitNass/wwwroot/js/site.js
@@ -2,3 +2,36 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function setTime() {
+    document.getElementById('currentTime').innerHTML = new Date().toDateString()
+}
+
+function callJcrop() {
+
+    var picture = $('#thepicture');  // Must be already loaded or cached!
+    picture.guillotine({ width: 600, height: 600 });
+    picture.guillotine('fit');
+
+    $('#rotate-left').click(function () {
+        picture.guillotine('rotateLeft');
+    });
+
+    $('#rotate-right').click(function () {
+        picture.guillotine('rotateLeft');
+    });
+
+    $('#zoom-in').click(function () {
+        picture.guillotine('zoomIn');
+    });
+
+    $('#zoom-out').click(function () {
+        picture.guillotine('zoomOut');
+    });
+
+    $('#fit').click(function () {
+        picture.guillotine('fit');
+    });
+}
+
