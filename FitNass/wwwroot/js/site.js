d73ac5a79336c568d0eab2a37e2c2f2a23a83b9d
mahmudunnabiRain <mahmudunnabirain@gmail.com>
2021-03-11 18:11:49 +0600

    image cropper bug fixed

--- FitNass/wwwroot/js/site.js
+++ FitNass/wwwroot/js/site.js
@@ -8,30 +8,59 @@ function setTime() {
     document.getElementById('currentTime').innerHTML = new Date().toDateString()
 }
 
-function callJcrop() {
 
-    var picture = $('#thepicture');  // Must be already loaded or cached!
-    picture.guillotine({ width: 600, height: 600 });
-    picture.guillotine('fit');
+function userIntroJS() {
 
-    $('#rotate-left').click(function () {
-        picture.guillotine('rotateLeft');
+
+    //profile picture cropper start\
+    function initiateCropper() {
+
+        var picture = $('#dpUploadedImage');  // Must be already loaded or cached!
+        picture.one('load', function () {
+            picture.guillotine({ width: 400, height: 400 });
+            picture.guillotine('fit');
+
+            $('#rotate-left').click(function () {
+                picture.guillotine('rotateLeft');
+            });
+
+            $('#rotate-right').click(function () {
+                picture.guillotine('rotateLeft');
+            });
+
+            $('#zoom-in').click(function () {
+                picture.guillotine('zoomIn');
+            });
+
+            $('#zoom-out').click(function () {
+                picture.guillotine('zoomOut');
+            });
+
+            $('#fit').click(function () {
+                picture.guillotine('fit');
+            });
+        });
+    }
+
+    initiateCropper();
+    //profile picture cropper end
+
+    //select an image and preview
+    $('#dpFileSelector').change(function () {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#dpUploadedImage').attr('src', e.target.result);
+                $('#dpUploadedImage').guillotine('remove');
+                initiateCropper();
+                document.getElementById("dpUploadStatus").innerHTML = "Drag Image To Set Position";
+            }
+            reader.readAsDataURL(this.files[0]);
+            
+        }
     });
 
-    $('#rotate-right').click(function () {
-        picture.guillotine('rotateLeft');
-    });
 
-    $('#zoom-in').click(function () {
-        picture.guillotine('zoomIn');
-    });
-
-    $('#zoom-out').click(function () {
-        picture.guillotine('zoomOut');
-    });
-
-    $('#fit').click(function () {
-        picture.guillotine('fit');
-    });
 }
 
